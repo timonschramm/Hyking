f2f1f3dda49181ea90bd89839d2a5018a8963bc9
Lorenz <lorenz.rasbach@t-online.de>
2024-11-21 01:59:11 +0100

    Added function to calculate hikes close to user || Added SQLite database file

--- recommender system/utils.py
+++ recommender system/utils.py
@@ -1,6 +1,7 @@
 from transformers import PreTrainedModel, PreTrainedTokenizer
 from sentence_transformers import SentenceTransformer
 import torch
+import sqlite3
 
 def get_text_embedding(text:str, model, tokenizer, avg:bool):
     """
@@ -31,3 +32,58 @@ def get_text_embedding(text:str, model, tokenizer, avg:bool):
         return embedding.numpy()
     else:
         ValueError("Model or Tokenizer invalidlse:VaModel or Tokenizer invalid")
+
+def get_closest_hikes(lat:int, lon:int, radius_km:int):
+    """
+     Finds the n closest hikes to the given position
+
+    Parameters:
+    - lat: Latitude
+    - lon: Longitude
+    - radius_km: Radius in kilometers
+
+    Returns:
+    - list: A list (sorted ascending by distance) of the ids of the hikes
+    """
+    conn = sqlite3.connect("Hike_SQL.db")
+    cursor = conn.cursor()
+
+    # Calculate a bounding box to reduce num number of hike distances that are calculated
+    min_lat = lat - radius_km
+    max_lat = lat + radius_km
+
+    min_lon = lon - radius_km
+    max_lon = lon + radius_km
+
+
+    query = """ 
+    SELECT 
+      id,
+
+      -- Calculating the Haversine Distance 
+      (6371 * 2 * 
+        ASIN(SQRT(
+            POWER(SIN(RADIANS(latitude - ?)/2), 2) +
+            COS(RADIANS(?)) * COS(RADIANS(latitude)) *
+            POWER(SIN(RADIANS(longitude - ?)/2), 2)
+        ))
+    ) AS distance
+    FROM
+        start_location
+    WHERE
+        latitude BETWEEN ? AND ?
+        AND longitude BETWEEN ? AND ?
+        AND distance <= ?
+    ORDER BY
+        distance;
+    
+"""
+
+    cursor.execute(query, (lat, lat, lon, min_lat, max_lat, min_lon, max_lon, radius_km, ))
+    results = cursor.fetchall()
+
+    cursor.close()
+    conn.close()
+
+    return results
+     
