7409657dfd5040299ccb5c0c91f63997bc28028c
Lorenz <lorenz.rasbach@t-online.de>
2024-11-25 01:55:51 +0100

    function: get_user_embedding gets user embedding from database || fix: changed descriptions

--- recommender system/utils.py
+++ recommender system/utils.py
@@ -2,19 +2,20 @@ from transformers import PreTrainedModel, PreTrainedTokenizer
 from sentence_transformers import SentenceTransformer
 import torch
 import sqlite3
+import numpy as np
 
 def get_text_embedding(text:str, model, tokenizer, avg:bool):
     """
     Calculate an embedding for the given text using the specified model
 
     Parameters:
-    - text: The text for which the embedding will be calculated
-    - model: The language model used for the embedding
-    - tokenizer: The according tokenizer for the model
+    - text: Text for which the embedding will be calculated
+    - model: Language model used for the embedding
+    - tokenizer: According tokenizer for the model
     - avg: If the cls embedding is used or if the embeddings are averaged
 
     Returns:
-    - numpy Array: The numpy array of the embedding for the given text
+    - numpy Array: Array of the embedding for the given text
     """
     if isinstance(model, SentenceTransformer):
         return torch.tensor(model.encode(text, convert_to_tensor=True)).numpy()
@@ -35,7 +36,7 @@ def get_text_embedding(text:str, model, tokenizer, avg:bool):
 
 def get_closest_hikes(lat:int, lon:int, radius_km:int):
     """
-     Finds the n closest hikes to the given position
+    Finds the hikes within radius of given position
 
     Parameters:
     - lat: Latitude
@@ -43,12 +44,13 @@ def get_closest_hikes(lat:int, lon:int, radius_km:int):
     - radius_km: Radius in kilometers
 
     Returns:
-    - list: A list (sorted ascending by distance) of the ids of the hikes
+    - list: IDs of the hikes (sorted ascending by distance) 
     """
     conn = sqlite3.connect("Hike_SQL.db")
     cursor = conn.cursor()
 
     # Calculate a bounding box to reduce num number of hike distances that are calculated
+    # TODO: change radius_km calculation by dividing by the right number
     min_lat = lat - radius_km
     max_lat = lat + radius_km
 
@@ -87,3 +89,28 @@ def get_closest_hikes(lat:int, lon:int, radius_km:int):
 
     return results
      
+
+def get_user_embedding(user_id:int):
+    """
+    Fetches the database for the user and returns an embedding of the relevant attributes
+    
+    Parameters:
+    - user_id: ID of the user
+
+    Returns:
+    - numpy Array: Array of the embedding
+    """
+
+    conn = sqlite3.connect("Hike_SQL.db")
+    cursor = conn.cursor()
+
+    attributes = ["stamina", "max_hike_time", "average_hike_time", "hike_count", "average_distance", "average_elevation_diff"] #TODO: Adjust attributes
+
+    query = f"SELECT {', '.join(attributes)} FROM users WHERE user_id = {user_id}"
+
+    cursor.execute(query)
+
+    user_attributes = cursor.fetchall()
+    conn.close()
+    user_embedding = np.array(user_attributes)
+    return user_embedding
