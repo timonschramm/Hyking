619937d48238312e335445b7f0057f2960873fb0
timonschramm <75097008+timonschramm@users.noreply.github.com>
2024-12-29 01:47:12 +0100

    python works again

--- nextjs-fastapi/next.config.js
+++ nextjs-fastapi/next.config.js
@@ -13,7 +13,7 @@ const nextConfig = {
         destination:
           process.env.NODE_ENV === "development"
             ? "http://127.0.0.1:8000/api/py/:path*"
-            : "/api/py/:path*",
+            : "/api/",
       },
     {
         source: "/docs",
