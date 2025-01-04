9e26cf21486a33ad984a7707faf4ed48eef62807
timonschramm <75097008+timonschramm@users.noreply.github.com>
2025-01-04 15:56:51 +0100

    fixed userrecs

--- nextjs-fastapi/app/api/userRecs/[userId]/route.ts
+++ nextjs-fastapi/app/api/userRecs/[userId]/route.ts
@@ -21,7 +21,11 @@ export async function GET(
       include: {
         artists: {
           include: {
-            genres: true
+            artist: {
+              include: {
+                genres: true
+              }
+            }
           }
         }
       }
