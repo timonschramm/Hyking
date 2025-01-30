3273a5f54c1148f1474d293f0af3789d5eb7107f
Nicolas-P-dev <80869612+Nicolas-P-dev@users.noreply.github.com>
2025-01-30 23:13:30 +0100

    Implemented Lorenz Solution
    Implemented Lorenz Solution

--- nextjs-fastapi/recommender_system/utils.py
+++ nextjs-fastapi/recommender_system/utils.py
@@ -1,156 +1,45 @@
-import sqlite3
 import numpy as np
-import openai
-import os
 from supabase import create_client, Client
 from dotenv import load_dotenv
+import os
 from collections import Counter
 
-# Load environment variables
 load_dotenv(".env.local")
 
-# Set OpenAI API key
-openai.api_key = os.getenv("OPENAI_API_KEY")
 
-def cosine_similarity(vec_a: np.ndarray, vec_b: np.ndarray) -> float:
+def get_user_skill_embedding(user_id: str):
     """
-    Computes the cosine similarity between two vectors.
+    Fetches the database for the user and returns an embedding of the user's hikingskills
 
     Parameters:
-    - vec_a: First vector.
-    - vec_b: Second vector.
+    - user_id: ID of the user
 
     Returns:
-    - float: Cosine similarity between the two vectors.
+    - numpy Array: Array of the embedding
     """
-    dot_product = np.dot(vec_a, vec_b.T)
-    norm_a = np.linalg.norm(vec_a)
-    norm_b = np.linalg.norm(vec_b)
-    return dot_product / (norm_a * norm_b)
 
-def get_text_embedding(text: str, model: str = "text-embedding-ada-002") -> np.ndarray:
-    """
-    Calculate an embedding for the given text using OpenAI's embeddings API.
-
-    Parameters:
-    - text: Text for which the embedding will be calculated.
-    - model: OpenAI embedding model to use (default is "text-embedding-ada-002").
-
-    Returns:
-    - numpy.ndarray: Array of the embedding for the given text.
-    """
-    try:
-        response = openai.Embedding.create(
-            input=text,
-            model=model
-        )
-        embedding = np.array(response["data"][0]["embedding"])
-        return embedding
-    except Exception as e:
-        raise ValueError(f"Failed to fetch embedding from OpenAI API: {e}")
-
-def get_closest_hikes(lat: float, lon: float, radius_km: float) -> list:
-    """
-    Finds the hikes within a radius of the given position.
-
-    Parameters:
-    - lat: Latitude.
-    - lon: Longitude.
-    - radius_km: Radius in kilometers.
-
-    Returns:
-    - list: IDs of the hikes (sorted ascending by distance).
-    """
-    conn = sqlite3.connect("Hike_SQL.db")
-    cursor = conn.cursor()
-
-    min_lat = lat - radius_km / 111.32
-    max_lat = lat + radius_km / 111.32
-    min_lon = lon - radius_km / (111.32 * np.cos(np.radians(lat)))
-    max_lon = lon + radius_km / (111.32 * np.cos(np.radians(lat)))
-
-    query = """
-    SELECT 
-      id,
-      (6371 * 2 * 
-        ASIN(SQRT(
-            POWER(SIN(RADIANS(latitude - ?)/2), 2) +
-            COS(RADIANS(?)) * COS(RADIANS(latitude)) *
-            POWER(SIN(RADIANS(longitude - ?)/2), 2)
-        ))
-      ) AS distance
-    FROM
-        start_location
-    WHERE
-        latitude BETWEEN ? AND ?
-        AND longitude BETWEEN ? AND ?
-        AND distance <= ?
-    ORDER BY
-        distance;
-    """
-    cursor.execute(query, (lat, lat, lon, min_lat, max_lat, min_lon, max_lon, radius_km))
-    results = cursor.fetchall()
-
-    cursor.close()
-    conn.close()
-
-    return results
-
-def get_user_embedding(user_id: int) -> np.ndarray:
-    """
-    Fetches the database for the user and returns an embedding of the relevant attributes.
-
-    Parameters:
-    - user_id: ID of the user.
-
-    Returns:
-    - numpy.ndarray: Array of the embedding.
-    """
-    conn = sqlite3.connect("Hike_SQL.db")
-    cursor = conn.cursor()
-
-    attributes = ["stamina", "max_hike_time", "average_hike_time", "hike_count", "average_distance", "average_elevation_diff"]
-    query = f"SELECT {', '.join(attributes)} FROM users WHERE user_id = ?"
-    cursor.execute(query, (user_id,))
-    user_attributes = cursor.fetchone()
-
-    cursor.close()
-    conn.close()
-
-    if user_attributes:
-        return np.array(user_attributes)
-    else:
-        raise ValueError("User not found")
-
-def get_user_skill_embedding(user_id: str) -> np.ndarray:
-    """
-    Fetches the database for the user and returns an embedding of the user's hiking skills.
-
-    Parameters:
-    - user_id: ID of the user.
-
-    Returns:
-    - numpy.ndarray: Array of the embedding.
-    """
     url: str = os.getenv("NEXT_PUBLIC_SUPABASE_URL")
     key: str = os.getenv("NEXT_PUBLIC_SUPABASE_ANON_KEY")
     supabase: Client = create_client(url, key)
 
-    attributes = ['experienceLevel', 'preferredPace', 'preferredDistance']
-    response = supabase.table("Profile").select(','.join(attributes)).eq("id", user_id).execute().data[0].values()
+    response = supabase.table("UserSkill").select("SkillLevel(numericValue, name)").eq("profileId", user_id).neq(
+        "SkillLevel.name", "CAR").neq("SkillLevel.name", "PUBLIC TRANSPORTATION").neq("SkillLevel.name",
+                                                                                      "BOTH").execute().data
 
-    skill_embedding = np.array(response).reshape(1, -1)
+    skills = [item['SkillLevel']['numericValue'] for item in response if item['SkillLevel'] is not None]
+    skill_embedding = np.array(skills).reshape(1, -1)
     return skill_embedding
 
-def get_user_direct_interest_embedding(user_id: int) -> np.ndarray:
+
+def get_user_direct_interest_embedding(user_id: int):
     """
-    Fetches the database for the user and returns an embedding of the user's interests.
+    Fetches the database for the user and returns an embedding of the user's interests
 
     Parameters:
-    - user_id: ID of the user.
+    - user_id: ID of the user
 
     Returns:
-    - numpy.ndarray: Array of the embedding.
+    - numpy Array: Array of the embedding
     """
     url: str = os.getenv("NEXT_PUBLIC_SUPABASE_URL")
     key: str = os.getenv("NEXT_PUBLIC_SUPABASE_ANON_KEY")
@@ -162,17 +51,19 @@ def get_user_direct_interest_embedding(user_id: int) -> np.ndarray:
     all_interests = [item["id"] for item in response]
 
     interest_embedding = np.array([1 if interest in user_interests else 0 for interest in all_interests], dtype=float)
+
     return interest_embedding.reshape(1, -1)
 
-def get_user_indirect_interest_embedding(user_id: int) -> np.ndarray:
+
+def get_user_indirect_interest_embedding(user_id: int):
     """
-    Fetches the database for the user and returns an embedding of the user's interest groups.
+    Fetches the database for the user and returns an embedding of the user's interest groups
 
     Parameters:
-    - user_id: ID of the user.
+    - user_id: ID of the user
 
     Returns:
-    - numpy.ndarray: Array of the embedding.
+    - numpy Array: Array of the embedding
     """
     url: str = os.getenv("NEXT_PUBLIC_SUPABASE_URL")
     key: str = os.getenv("NEXT_PUBLIC_SUPABASE_ANON_KEY")
@@ -189,87 +80,98 @@ def get_user_indirect_interest_embedding(user_id: int) -> np.ndarray:
     interest_embedding = np.array(result).reshape(1, -1)
     return interest_embedding
 
-def calc_skill_similarity(user_id_a: int, user_id_b: int) -> float:
+
+def calc_skill_similarity(user_id_a: int, user_id_b: int):
     """
-    Calculates the skill similarity between two users.
+    Calculates the skill similarity between two users
 
     Parameters:
-    - user_id_a: ID of the first user.
-    - user_id_b: ID of the second user.
+    - user_id_a: ID of the first user
+    - user_id_b: ID of the second user
 
     Returns:
-    - float: Skill similarity between the two users.
+    - float: Skill similarity between the two users
     """
+
     user_a_skill_embedding = get_user_skill_embedding(user_id_a)
     user_b_skill_embedding = get_user_skill_embedding(user_id_b)
 
-    return cosine_similarity(user_a_skill_embedding, user_b_skill_embedding)
+    try:
+        return cosine_similarity_numpy(user_a_skill_embedding, user_b_skill_embedding)[0][0]
+    except Exception:
+        return 0
 
-def calc_interest_similarity(user_id_a: int, user_id_b: int) -> float:
+
+def calc_interest_similarity(user_id_a: int, user_id_b: int):
     """
-    Calculates the interest similarity between two users.
+    Calculates the interest similarity between two users
 
     Parameters:
-    - user_id_a: ID of the first user.
-    - user_id_b: ID of the second user.
+    - user_id_a: ID of the first user
+    - user_id_b: ID of the second user
 
     Returns:
-    - float: Interest similarity between the two users.
+    - float: Interest similarity between the two users
     """
     user_a_direct_interest_embedding = get_user_direct_interest_embedding(user_id_a)
     user_b_direct_interest_embedding = get_user_direct_interest_embedding(user_id_b)
-    direct_interest_sim = cosine_similarity(user_a_direct_interest_embedding, user_b_direct_interest_embedding)
+    direct_interest_sim = \
+    cosine_similarity_numpy(user_a_direct_interest_embedding, user_b_direct_interest_embedding)[0][0]
 
     user_a_indirect_interest_embedding = get_user_indirect_interest_embedding(user_id_a)
     user_b_indirect_interest_embedding = get_user_indirect_interest_embedding(user_id_b)
-    indirect_interest_sim = cosine_similarity(user_a_indirect_interest_embedding, user_b_indirect_interest_embedding)
+    indirect_interest_sim = \
+    cosine_similarity_numpy(user_a_indirect_interest_embedding, user_b_indirect_interest_embedding)[0][0]
 
     return 0.33 * direct_interest_sim + 0.67 * indirect_interest_sim
 
-def calc_hike_description_similarity(hike_desc_a: str, hike_desc_b: str) -> float:
+
+def cosine_similarity_numpy(vec1, vec2):
+    # Flatten if 2D (assumes shape (1, N))
+    vec1, vec2 = vec1.flatten(), vec2.flatten()
+
+    # Handle empty vectors (return similarity of 0)
+    if vec1.size == 0 or vec2.size == 0:
+        return 0.0
+
+    # Compute dot product and norms
+    dot_product = np.dot(vec1, vec2)
+    norm_vec1 = np.linalg.norm(vec1)
+    norm_vec2 = np.linalg.norm(vec2)
+
+    # Handle division by zero
+    return dot_product / (norm_vec1 * norm_vec2) if norm_vec1 * norm_vec2 != 0 else 0.0
+
+
+def calc_overall_similarity(user_id_a: int, user_id_b: int, hike_desc_a: str, hike_desc_b: str):
     """
-    Calculates the similarity between two hike descriptions.
+    Calculates the overall similarity between two users and the respective hike descriptions
 
     Parameters:
-    - hike_desc_a: Description of the first hike.
-    - hike_desc_b: Description of the second hike.
+    - user_id_a: ID of the first user
+    - user_id_b: ID of the second user
+    - hike_desc_a: Description of the first hike
+    - hike_desc_b: Description of the second hike
 
     Returns:
-    - float: Similarity between the two hikes.
+    - float: Overall similarity between the two users and the respective hikes
     """
-    hike_desc_a_embedding = get_text_embedding(hike_desc_a)
-    hike_desc_b_embedding = get_text_embedding(hike_desc_b)
+    return (0.66 * calc_skill_similarity(user_id_a, user_id_b)
+            + 0.34 * calc_interest_similarity(user_id_a, user_id_b))  # TODO: Discuss weights
 
-    return cosine_similarity(hike_desc_a_embedding, hike_desc_b_embedding)
 
-def calc_overall_similarity(user_id_a: int, user_id_b: int, hike_desc_a: str, hike_desc_b: str) -> float:
+def get_recommendations(user_id: int, hike_desc: str):
     """
-    Calculates the overall similarity between two users and the respective hike descriptions.
+    Calculates a list of recommendations for the user based on skill, interests and given hike description
 
     Parameters:
-    - user_id_a: ID of the first user.
-    - user_id_b: ID of the second user.
-    - hike_desc_a: Description of the first hike.
-    - hike_desc_b: Description of the second hike.
+    - user_id: ID of the user
+    - hike_desc: Description of the hike
 
     Returns:
-    - float: Overall similarity between the two users and the respective hikes.
+    - list of int: IDs of recommended users
     """
-    return (0.45 * calc_skill_similarity(user_id_a, user_id_b)
-            + 0.35 * calc_interest_similarity(user_id_a, user_id_b)
-            + 0.2 * calc_hike_description_similarity(hike_desc_a, hike_desc_b))
 
-def get_recommendations(user_id: int, hike_desc: str) -> list:
-    """
-    Calculates a list of recommendations for the user based on skill, interests, and given hike description.
-
-    Parameters:
-    - user_id: ID of the user.
-    - hike_desc: Description of the hike.
-
-    Returns:
-    - list: IDs of recommended users.
-    """
     url: str = os.getenv("NEXT_PUBLIC_SUPABASE_URL")
     key: str = os.getenv("NEXT_PUBLIC_SUPABASE_ANON_KEY")
     supabase: Client = create_client(url, key)
@@ -283,26 +185,10 @@ def get_recommendations(user_id: int, hike_desc: str) -> list:
     ids = [id for id in all_ids if id not in swiped_users]
     sim_list = []
     for id in ids:
-        sim = calc_interest_similarity(user_id, id)  # TODO: Change to overall similarity
+        sim = calc_overall_similarity(user_id, id)
         sim_list.append((id, sim))
 
     sim_list.sort(key=lambda x: x[1], reverse=True)
 
-    return [tuple[0] for tuple in sim_list[:7]]  # TODO: Change to 10
+    return [tuple[0] for tuple in sim_list[:10]]
 
-def calc_intermediate_similarity(user_id_a: str, user_id_b: str) -> float:
-    """
-    Calculates the intermediate similarity between the user and all other users.
-
-    Parameters:
-    - user_id_a: ID of the first user.
-    - user_id_b: ID of the second user.
-
-    Returns:
-    - float: Intermediate similarity between the user and all other users.
-    """
-    interest_sim = calc_interest_similarity(user_id_a, user_id_b)
-    skill_sim = calc_skill_similarity(user_id_a, user_id_b)
-    intermediate_similarity = 0.35 * interest_sim + 0.45 * skill_sim  # TODO: Discuss weights
-
-    return intermediate_similarity
