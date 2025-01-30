97c8dcda098033b4a0d9b05ccc2d2a57229ac94c
Lorenz <lorenz.rasbach@t-online.de>
2025-01-30 22:45:20 +0100

    Cleanup Repo to ensure hostability

--- recommender_system/utils.py
+++ recommender_system/utils.py
@@ -1,6 +1,3 @@
-from transformers import PreTrainedModel, PreTrainedTokenizer
-from sentence_transformers import SentenceTransformer
-import torch
 import sqlite3
 import numpy as np
 import math
@@ -11,116 +8,6 @@ import os
 from collections import Counter
 
 load_dotenv(".env.local")
-
-def get_text_embedding(text:str, model, tokenizer, avg:bool):
-    """
-    Calculate an embedding for the given text using the specified model
-
-    Parameters:
-    - text: Text for which the embedding will be calculated
-    - model: Language model used for the embedding
-    - tokenizer: According tokenizer for the model
-    - avg: If the cls embedding is used or if the embeddings are averaged
-
-    Returns:
-    - numpy Array: Array of the embedding for the given text
-    """
-    if isinstance(model, SentenceTransformer):
-        return torch.tensor(model.encode(text, convert_to_tensor=True)).numpy()
-    elif isinstance(model, PreTrainedModel) and isinstance(tokenizer, PreTrainedTokenizer):
-        input = tokenizer(text, return_tensors="pt", truncation=True, padding=True)
-
-        with torch.no_grad():
-            output = model(**input)
-
-        if avg:
-            embedding = output.last_hidden_state.mean(dim=1)
-        else:
-            embedding = output.last_hidden_state[:, 0, :]
-        
-        return embedding.numpy()
-    else:
-        ValueError("Model or Tokenizer invalidlse:VaModel or Tokenizer invalid")
-
-def get_closest_hikes(lat:int, lon:int, radius_km:int):
-    """
-    Finds the hikes within radius of given position
-
-    Parameters:
-    - lat: Latitude
-    - lon: Longitude
-    - radius_km: Radius in kilometers
-
-    Returns:
-    - list: IDs of the hikes (sorted ascending by distance) 
-    """
-    conn = sqlite3.connect("Hike_SQL.db")
-    cursor = conn.cursor()
-
-    # Calculate a bounding box to reduce num number of hike distances that are calculated
-    min_lat = lat - (radius_km / 111)
-    max_lat = lat + (radius_km / 111)
-
-    min_lon = lon - (radius_km / (111 * math.cos(math.radians(lat))))
-    max_lon = lon + (radius_km / (111 * math.cos(math.radians(lat))))
-
-
-    query = """ 
-    SELECT 
-      id,
-
-      -- Calculating the Haversine Distance 
-      (6371 * 2 * 
-        ASIN(SQRT(
-            POWER(SIN(RADIANS(latitude - ?)/2), 2) +
-            COS(RADIANS(?)) * COS(RADIANS(latitude)) *
-            POWER(SIN(RADIANS(longitude - ?)/2), 2)
-        ))
-    ) AS distance
-    FROM
-        start_location
-    WHERE
-        latitude BETWEEN ? AND ?
-        AND longitude BETWEEN ? AND ?
-        AND distance <= ?
-    ORDER BY
-        distance;
-    
-"""
-
-    cursor.execute(query, (lat, lat, lon, min_lat, max_lat, min_lon, max_lon, radius_km, ))
-    results = cursor.fetchall()
-
-    cursor.close()
-    conn.close()
-
-    return results
-     
-
-def get_user_embedding(user_id:int):
-    """
-    Fetches the database for the user and returns an embedding of the relevant attributes
-    
-    Parameters:
-    - user_id: ID of the user
-
-    Returns:
-    - numpy Array: Array of the embedding
-    """
-
-    conn = sqlite3.connect("Hike_SQL.db")
-    cursor = conn.cursor()
-
-    attributes = ["stamina", "max_hike_time", "average_hike_time", "hike_count", "average_distance", "average_elevation_diff"]
-
-    query = f"SELECT {', '.join(attributes)} FROM users WHERE user_id = {user_id}"
-
-    cursor.execute(query)
-
-    user_attributes = cursor.fetchall()
-    conn.close()
-    user_embedding = np.array(user_attributes)
-    return user_embedding
 
 def get_user_skill_embedding(user_id:str):
     """
@@ -137,10 +24,10 @@ def get_user_skill_embedding(user_id:str):
     key: str = os.getenv("NEXT_PUBLIC_SUPABASE_ANON_KEY")
     supabase: Client = create_client(url, key)
 
-    attributes = ['experienceLevel','preferredPace','preferredDistance']  #TODO: Might change
-    response = supabase.table("Profile").select(','.join(attributes)).eq("id", user_id).execute().data[0].values()
+    response = supabase.table("UserSkill").select("SkillLevel(numericValue, name)").eq("profileId", user_id).neq("SkillLevel.name", "CAR").neq("SkillLevel.name", "PUBLIC TRANSPORTATION").neq("SkillLevel.name", "BOTH").execute().data
 
-    skill_embedding = np.array(response).reshape(1, -1)
+    skills = [item['SkillLevel']['numericValue'] for item in response if item['SkillLevel'] is not None]
+    skill_embedding = np.array(skills).reshape(1, -1) 
     return skill_embedding
 
 def get_user_direct_interest_embedding(user_id:int):
@@ -208,7 +95,10 @@ def calc_skill_similarity(user_id_a:int, user_id_b:int):
     user_a_skill_embedding = get_user_skill_embedding(user_id_a)
     user_b_skill_embedding = get_user_skill_embedding(user_id_b)
 
-    return cosine_similarity(user_a_skill_embedding, user_b_skill_embedding)[0][0]
+    try:
+        return cosine_similarity(user_a_skill_embedding, user_b_skill_embedding)[0][0]
+    except Exception:
+        return 0
 
 def calc_interest_similarity(user_id_a:int, user_id_b:int):
     """
@@ -231,21 +121,6 @@ def calc_interest_similarity(user_id_a:int, user_id_b:int):
 
     return 0.33 * direct_interest_sim + 0.67 * indirect_interest_sim
 
-def calc_hike_description_similarity(hike_desc_a:str, hike_desc_b:str):
-    """
-    Calculates the similarity between two hike descriptions
-
-    Parameters:
-    - hike_desc_a: Description of the first hike
-    - hike_desc_b: Description of the second hike
-
-    Returns:
-    - float: Similarity between the two hikes
-    """
-    hike_desc_a_embedding = get_text_embedding(hike_desc_a)
-    hike_desc_b_embedding = get_text_embedding(hike_desc_b)
-
-    return cosine_similarity(hike_desc_a_embedding, hike_desc_b_embedding)
 
 def calc_overall_similarity(user_id_a:int, user_id_b:int, hike_desc_a:str, hike_desc_b:str):
     """
@@ -260,11 +135,10 @@ def calc_overall_similarity(user_id_a:int, user_id_b:int, hike_desc_a:str, hike_
     Returns:
     - float: Overall similarity between the two users and the respective hikes
     """
-    return (0.45 * calc_skill_similarity(user_id_a, user_id_b) 
-            + 0.35 * calc_interest_similarity(user_id_a, user_id_b) 
-            + 0.2 * calc_hike_description_similarity(hike_desc_a, hike_desc_b)) #TODO: Discuss weights
+    return (0.66 * calc_skill_similarity(user_id_a, user_id_b) 
+            + 0.34 * calc_interest_similarity(user_id_a, user_id_b) ) #TODO: Discuss weights
 
-def get_recommendations(user_id:int, hike_desc:str): #TODO: Implement
+def get_recommendations(user_id:int, hike_desc:str): 
     """
     Calculates a list of recommendations for the user based on skill, interests and given hike description
 
@@ -289,31 +163,9 @@ def get_recommendations(user_id:int, hike_desc:str): #TODO: Implement
     ids = [id for id in all_ids if id not in swiped_users]
     sim_list = []
     for id in ids:
-        sim = calc_interest_similarity(user_id, id) #TODO: Change to overall similarity
+        sim = calc_overall_similarity(user_id, id) #TODO: Change to overall similarity
         sim_list.append((id, sim))
 
     sim_list.sort(key=lambda x: x[1], reverse=True)
 
     return [tuple[0] for tuple in sim_list[:7]] #TODO: Change to 10
-
-def calc_intermediate_similarity(user_id_a:str, user_id_b:str):
-    """
-    Calculates the intermediate similarity between the user and all other users
-    
-    Parameters:
-    - user_id_a: ID of the first user
-    - user_id_b: ID of the second user
-    
-    Returns:
-    - float: Intermediate similarity between the user and all other users
-    """
-
-    url:str = os.getenv("NEXT_PUBLIC_SUPABASE_URL")
-    key: str = os.getenv("NEXT_PUBLIC_SUPABASE_ANON_KEY")
-    supabase: Client = create_client(url, key)
-
-    interest_sim = calc_interest_similarity(user_id_a, user_id_b)
-    skill_sim = calc_skill_similarity(user_id_a, user_id_b)
-    intermediate_similarity = 0.35 * interest_sim + 0.45 * skill_sim #TODO: Discuss weights
-
-    return intermediate_similarity
