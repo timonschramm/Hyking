22a4bae5d37bdd51325b9613a56201ed17e84a99
Nicolas-P-dev <80869612+Nicolas-P-dev@users.noreply.github.com>
2025-01-31 16:02:13 +0100

    moved _recommender_system and adjusted package.json python3 to python
    Moved recommender_systen

--- nextjs-fastapi/api/getRecs.py
+++ nextjs-fastapi/api/getRecs.py
@@ -5,10 +5,11 @@ from fastapi import APIRouter
 
 # Add the parent directory to sys.path so Python can find the recommender_system module
 sys.path.append(os.path.dirname(os.path.dirname(os.path.dirname(__file__))))
-from recommender_system.utils import get_recommendations
+from .recommender_system.utils import get_recommendations
+
 
 # Erstelle einen Router
-router = APIRouter() 
+router = APIRouter()
 
 
 @router.get("/recommendations")
@@ -16,4 +17,4 @@ async def get_recommendation(userID: str): #TODO: Add hike_desc
     # For now, we'll just return the userID
     print("get_recommendation called in the python api")
     rec_ids = get_recommendations(userID, "") #Change
-    return {"recommendedUserIDs": rec_ids} 
+    return {"recommendedUserIDs": rec_ids}
